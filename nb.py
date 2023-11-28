3441ca247c19f10baa9777a1fafc8057e8ba481c
lydializhenzhenemory <123418371+lydializhenzhenemory@users.noreply.github.com>
2023-11-27 21:39:51 -0500

    Update nb.py

--- nb.py
+++ nb.py
@@ -1,37 +1,53 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, roc_auc_score, f1_score
+import numpy as np
 
 # Load the datasets
 x_features = pd.read_csv('normalized_x_features.csv')
 y_target = pd.read_csv('y_target_binary.csv')
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(x_features, y_target.values.ravel(), test_size=0.3, random_state=42)
+# Define the number of folds for cross-validation
+n_folds = 5
+kf = KFold(n_splits=n_folds, shuffle=True, random_state=42)
 
 # List of variance smoothing values to try
 var_smoothing_list = [1e-9, 1e-8, 1e-7, 1e-6, 1e-5]
 
 # Iterate over the variance smoothing values
 for var_smoothing in var_smoothing_list:
-    # Initialize the Gaussian Naive Bayes model with the current var_smoothing
-    nb_classifier = GaussianNB(var_smoothing=var_smoothing)
+    accuracy_scores = []
+    auroc_scores = []
+    f1_scores = []
+    
+    # Iterate over each fold
+    for train_index, test_index in kf.split(x_features):
+        X_train, X_test = x_features.iloc[train_index], x_features.iloc[test_index]
+        y_train, y_test = y_target.values.ravel()[train_index], y_target.values.ravel()[test_index]
 
-    # Train the model
-    nb_classifier.fit(X_train, y_train)
+        # Initialize the Gaussian Naive Bayes model with the current var_smoothing
+        nb_classifier = GaussianNB(var_smoothing=var_smoothing)
 
-    # Predictions on the test set
-    y_pred_nb = nb_classifier.predict(X_test)
-    y_pred_proba_nb = nb_classifier.predict_proba(X_test)[:, 1] # Probabilities for the positive class
+        # Train the model
+        nb_classifier.fit(X_train, y_train)
 
-    # Calculate accuracies and AUROC
-    accuracy_nb = accuracy_score(y_test, y_pred_nb)
-    auroc_nb = roc_auc_score(y_test, y_pred_proba_nb)
-    f1_score_nb = f1_score(y_test, y_pred_nb)  # Calculate F1 score
+        # Predictions on the test set
+        y_pred_nb = nb_classifier.predict(X_test)
+        y_pred_proba_nb = nb_classifier.predict_proba(X_test)[:, 1]  # Probabilities for the positive class
+
+        # Calculate and append accuracies and AUROC
+        accuracy_scores.append(accuracy_score(y_test, y_pred_nb))
+        auroc_scores.append(roc_auc_score(y_test, y_pred_proba_nb))
+        f1_scores.append(f1_score(y_test, y_pred_nb))
+
+    # Calculate average scores across all folds
+    avg_accuracy = np.mean(accuracy_scores)
+    avg_auroc = np.mean(auroc_scores)
+    avg_f1_score = np.mean(f1_scores)
 
     # Print the results
     print(f"Naive Bayes Classifier with var_smoothing={var_smoothing} ---------------")
-    print("Test Accuracy:", accuracy_nb)
-    print("AUROC:", auroc_nb)
-    print("F1 Score:", f1_score_nb)
+    print("Average Test Accuracy:", avg_accuracy)
+    print("Average AUROC:", avg_auroc)
+    print("Average F1 Score:", avg_f1_score)
