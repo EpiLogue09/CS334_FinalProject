488dd9e76ef448f458f49ee6c0349cd672774543
lydializhenzhenemory <123418371+lydializhenzhenemory@users.noreply.github.com>
2023-11-27 21:11:10 -0500

    Update nb.py

--- nb.py
+++ nb.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 # Load the datasets
 x_features = pd.read_csv('normalized_x_features.csv')
@@ -10,21 +10,28 @@ y_target = pd.read_csv('y_target_binary.csv')
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(x_features, y_target.values.ravel(), test_size=0.3, random_state=42)
 
-# Initialize the Gaussian Naive Bayes model
-nb_classifier = GaussianNB()
+# List of variance smoothing values to try
+var_smoothing_list = [1e-9, 1e-8, 1e-7, 1e-6, 1e-5]
 
-# Train the model
-nb_classifier.fit(X_train, y_train)
+# Iterate over the variance smoothing values
+for var_smoothing in var_smoothing_list:
+    # Initialize the Gaussian Naive Bayes model with the current var_smoothing
+    nb_classifier = GaussianNB(var_smoothing=var_smoothing)
 
-# Predictions on the training and test set
-y_pred_train_nb = nb_classifier.predict(X_train)
-y_pred_nb = nb_classifier.predict(X_test)
+    # Train the model
+    nb_classifier.fit(X_train, y_train)
 
-# Calculate accuracies
-accuracy_train_nb = accuracy_score(y_train, y_pred_train_nb)
-accuracy_nb = accuracy_score(y_test, y_pred_nb)
+    # Predictions on the test set
+    y_pred_nb = nb_classifier.predict(X_test)
+    y_pred_proba_nb = nb_classifier.predict_proba(X_test)[:, 1] # probabilities for the positive class
 
-# Print the results
-print("Naive Bayes Classifier ---------------")
-print("Training Accuracy:", accuracy_train_nb)
-print("Test Accuracy:", accuracy_nb)
+    # Calculate accuracies
+    accuracy_nb = accuracy_score(y_test, y_pred_nb)
+
+    # Calculate AUROC
+    auroc_nb = roc_auc_score(y_test, y_pred_proba_nb)
+
+    # Print the results
+    print(f"Naive Bayes Classifier with var_smoothing={var_smoothing} ---------------")
+    print("Test Accuracy:", accuracy_nb)
+    print("AUROC:", auroc_nb)
